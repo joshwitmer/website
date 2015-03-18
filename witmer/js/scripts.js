9db4268728e4aee094f74d75fd970156c503d294
Josh Witmer <jsw5111@gmail.com>
2015-03-18 10:57:32 -0400

    Initial check in

---
+++ witmer/js/scripts.js
@@ -0,0 +1,13 @@
+/* Smooth scrolling action */
+$(document).ready(function(){
+	$(".js-next-section").click(function(e){
+		e.preventDefault();
+
+		var dest = $(this).attr("href");
+
+		$("html, body").animate({
+			scrollTop: $(dest).offset().top
+		}, 400);
+	});
+});
+
